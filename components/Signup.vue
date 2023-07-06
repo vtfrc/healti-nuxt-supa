f2132161f7ef36f33201b3dedf4586fb205cdfbb
Vittorio Faraco <info@vittoriofaraco.com>
2023-07-06 15:00:44 +0200

    auth styling

--- components/Signup.vue
+++ components/Signup.vue
@@ -30,16 +30,16 @@ const login = async () => {
         <input class="inputField rounded-md border px-4 py-2 mt-2 w-full dark:bg-[#202020] dark:border-[#282828] dark:text-white" type="email" placeholder="Your email" v-model="email" />
         <input class="inputField rounded-md border px-4 py-2 mt-2 w-full dark:bg-[#202020] dark:border-[#282828] dark:text-white" type="password" placeholder="Your password" v-model="password" />
       </div>
-      <div class="flex justify-between">
+      <div class="text-center md:text-left md:flex md:justify-between">
         <UButton
           variant="transparent"
           @click="login"
         >
-          <p class="mt-3">Already have an account? Log in</p>
+          <p class="mt-3 text-[#87AA9E]">Already have an account? Log in</p>
         </UButton>
         <input
           type="submit"
-          class="button bg-[#64CFAC] text-white px-4 py-2 rounded-md mt-3 float-right"
+          class="button bg-[#64CFAC] text-white px-4 py-2 rounded-md mt-3 md:float-right"
           :value="loading ? 'Loading' : 'Sign up'"
           :disabled="loading"
         />
