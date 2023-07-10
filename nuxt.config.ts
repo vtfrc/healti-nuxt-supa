aca126660e539d9152ac35aad932dade683c5ecb
Vittorio Faraco <info@vittoriofaraco.com>
2023-07-10 08:40:04 +0200

    favicon for apple devices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,5 +19,19 @@ export default defineNuxtConfig({
   },
   colorMode: {
     classSuffix: '',
-  }
+  },
+  app: {
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      ],
+      link: [
+        { rel: 'apple-touch-icon', type: 'image/png', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
 })
