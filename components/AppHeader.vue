02e2d1961fac36f59b1f5b8738217103efacb85c
Vittorio Faraco <info@vittoriofaraco.com>
2023-07-14 09:21:42 +0200

    dark mode icon default

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -41,9 +41,9 @@ const account = async () => {
           class="flex u-text-white transition-all duration-500 ease-in-out"
           @click="toggleDark"
         > 
-          <Icon v-if="colorMode.preference === 'dark'" name="ph:sun-fill" />
+
           <Icon v-if="colorMode.value === 'dark'" name="ph:sun-fill" />
-          <Icon v-if="colorMode.preference === 'light'" name="ph:moon-fill" />
+
           <Icon v-if="colorMode.value === 'light'" name="ph:moon-fill" />
         </button>
         <button
