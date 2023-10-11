e4241658e988636c11fb0288800f69f88045d586
Vittorio Faraco <faracovittorio@gmail.com>
2023-10-11 18:06:52 +0200

    minor fix

--- components/PageContainer.vue
+++ components/PageContainer.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="min-h-screen px-4 mx-auto sm:px-6 lg:px-8">
-    <div class="min-h-screen -mt-[50px] flex items-center justify-center">
+    <div class="min-h-screen lg:-mt-[50px] mt-[50px] pb-[50px] flex items-center justify-center">
     <slot />
     </div>
 </div>
