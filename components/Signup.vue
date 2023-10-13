7672594728e499a999c878614a6e6d017f99648f
Vittorio Faraco <faracovittorio@gmail.com>
2023-10-13 11:05:35 +0200

    signup msgs and fix

--- components/Signup.vue
+++ components/Signup.vue
@@ -4,14 +4,24 @@ const supabase = useSupabaseClient()
 const email = ref('')
 const password = ref('')
 
+const isLoading = ref(false)
+const successMsg = ref('')
+const errorMsg = ref('')
+
 const handleSignUp = async () => {
-  const { user, error } = await supabase.auth.signUp({
-    email: email.value,
-    password: password.value
-  })
-  console.log(user)
-  console.log(error)
-  navigateTo('/dashboard')
+  try {
+    isLoading.value = true
+    const { user, error } = await supabase.auth.signUp({
+      email: email.value,
+      password: password.value
+    })
+    if (error) throw error
+    successMsg.value = 'Check your email to confirm your account.'
+    isLoading.value = false
+  } catch (error) {
+    errorMsg.value = error.message
+    isLoading.value = false
+  }
 }
 
 const login = async () => {
@@ -22,6 +32,12 @@ const login = async () => {
 <template>
   <form class="form-widget flex flex-col max-w-[500px] w-full" @submit.prevent="handleSignUp">
       <h1 class="description font-semibold text-3xl text-center mb-4 dark:text-white">Sign up for Healti.</h1>
+      <div class="rounded-md mb-2 border bg-[#f5b8b8] border-[#b56262] dark:bg-[#692b2b] dark:border-[#a84545]" v-if="errorMsg">
+        <p class="text-center dark:text-white">{{ errorMsg }}</p>
+      </div>
+      <div class="rounded-md mb-2 border bg-[#b8f5c4] border-[#62b562] dark:bg-[#2b6952] dark:border-[#45a845]" v-if="successMsg">
+        <p class="text-center dark:text-white">{{ successMsg }}</p>
+      </div>
       <div class="flex flex-col w-full">
         <input class="inputField rounded-md border px-4 py-2 mt-2 w-full dark:bg-[#202020] dark:border-[#282828] dark:text-white" type="email" placeholder="Your email" v-model="email" />
         <input class="inputField rounded-md border px-4 py-2 mt-2 w-full dark:bg-[#202020] dark:border-[#282828] dark:text-white" type="password" placeholder="Your password" v-model="password" />
@@ -36,8 +52,9 @@ const login = async () => {
         </button>
         <input
           type="submit"
-          class="button cursor-pointer bg-[#64CFAC] text-white px-4 py-2 rounded-md mt-3 md:float-right"
-          value="Sign up"
+          :class="`button cursor-pointer bg-[#64CFAC] text-white px-4 py-2 rounded-md mt-3 md:float-right`"
+          :value="isLoading ? 'Signing you up...' : 'Sign up'"
+          :disabled="isLoading"
         />
       </div>
   </form>
