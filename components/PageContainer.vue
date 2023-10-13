76a71088df2f7890371012c6fe5c7b907de63b44
Vittorio Faraco <faracovittorio@gmail.com>
2023-10-13 12:34:22 +0200

    sizing fix

--- components/PageContainer.vue
+++ components/PageContainer.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="min-h-[90vh] px-4 mx-auto sm:px-6 lg:px-8">
+<div class="min-h-[90vh] px-4 mx-auto sm:px-6 lg:px-8 flex flex-col justify-center">
     <div class="flex items-center justify-center">
         <slot />
     </div>
